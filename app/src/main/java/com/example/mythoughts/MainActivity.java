2e9eb2675e992273da47e26b524884446a72c8d0
Vinay Vincent <vinayvincent15@gmail.com>
2022-06-05 02:53:19 +0530

    Created "Add Thought" button
    Add Thought stores the thought with its title to the firestore/Journal/First Thoughts.

--- app/src/main/java/com/example/mythoughts/MainActivity.java
+++ app/src/main/java/com/example/mythoughts/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.mythoughts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText ETtitle,ETthought;
+    private Button save;
+    private TextView show;
+    private static final String keytitle="Title", keythought="Thought";
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ETtitle = findViewById(R.id.ETtitle);
+        ETthought = findViewById(R.id.ETthought);
+        save = findViewById(R.id.Btnadd);
+        show = findViewById(R.id.TVshow);
+
+        save.setOnClickListener(v -> {
+            String title = ETtitle.getText().toString().trim();
+            String thought = ETthought.getText().toString().trim();
+            Map<String,Object> user = new HashMap<>();
+            user.put(keytitle, title);
+            user.put(keythought, thought);
+            db.collection("Journal")
+                    .document("First Thoughts")
+                    .set(user)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(MainActivity.this,"Successful",Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this,"Failed",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
     }
 }
