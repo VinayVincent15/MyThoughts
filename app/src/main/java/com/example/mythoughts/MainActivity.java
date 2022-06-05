a0d31d3f89db86f07839917604ceefd1118d0bb6
Vinay Vincent <vinayvincent15@gmail.com>
2022-06-05 12:07:38 +0530

    Created "Show Thoughts" button
    -> Show Thoughts retrieve the data present in Journal/First Thoughts.
    -> Added app icon.
    -> Removed Action bar

--- app/src/main/java/com/example/mythoughts/MainActivity.java
+++ app/src/main/java/com/example/mythoughts/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -19,10 +20,12 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private EditText ETtitle,ETthought;
-    private Button save;
-    private TextView show;
+    private Button save,show;
+    private TextView TVrectitle,TVrecthought;
     private static final String keytitle="Title", keythought="Thought";
+
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference journalref = db.document("Journal/First Thoughts");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,9 @@ public class MainActivity extends AppCompatActivity {
         ETtitle = findViewById(R.id.ETtitle);
         ETthought = findViewById(R.id.ETthought);
         save = findViewById(R.id.Btnadd);
-        show = findViewById(R.id.TVshow);
+        TVrectitle = findViewById(R.id.TVrectitle);
+        TVrecthought = findViewById(R.id.TVrecthought);
+        show = findViewById(R.id.Btnshow);
 
         save.setOnClickListener(v -> {
             String title = ETtitle.getText().toString().trim();
@@ -39,9 +44,7 @@ public class MainActivity extends AppCompatActivity {
             Map<String,Object> user = new HashMap<>();
             user.put(keytitle, title);
             user.put(keythought, thought);
-            db.collection("Journal")
-                    .document("First Thoughts")
-                    .set(user)
+            journalref.set(user)
                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void unused) {
@@ -52,6 +55,29 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onFailure(@NonNull Exception e) {
                             Toast.makeText(MainActivity.this,"Failed",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
+
+        show.setOnClickListener(v -> {
+            db.collection("Journal").document("First Thoughts")
+                    .get()
+                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if(documentSnapshot.exists()){
+                                TVrectitle.setText(documentSnapshot.getString(keytitle));
+                                TVrecthought.setText(documentSnapshot.getString(keythought));
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this,"No data",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this,"Failed to get data",Toast.LENGTH_SHORT).show();
                         }
                     });
         });
